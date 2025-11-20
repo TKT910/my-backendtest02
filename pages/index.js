4733482f11dd90f941bf940aacf801b2be36f3d5
Kotaro Egawa <KotaroEgawa@Katsu.local>
2025-11-21 00:24:35 +0900

    Add top page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>ようこそ、私のサイトへ！</h1>
+      <p>ここがトップページです。</p>
+    </div>
+  )
+}
